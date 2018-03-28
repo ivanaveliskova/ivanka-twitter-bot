bff93ba71f378083f42767df07ab4017a26e2aec
Ivana Veliskova <iveliskova@getguru.com>
2018-03-28 15:27:18 -0400

    Comments out hi replies and following

--- twitterBot.js
+++ twitterBot.js
@@ -38,78 +38,78 @@ var Tweet = new Twit(secret);
 
 // This should be tracking/listening for when things occur and then do something with that data.
 
-var stream = Tweet.stream('statuses/filter', { track: '@ivankabot' });
+// var stream = Tweet.stream('statuses/filter', { track: '@ivankabot' });
 
-stream.on('tweet', function (tweet) {
+// stream.on('tweet', function (tweet) {
 
-    var username = '@' + tweet.user.screen_name,
-        tweetText = tweet.text,
-        regexHello = /hello/i,
-        regexHi = /hi/i,
-        regexHey = /hey/i;
+//     var username = '@' + tweet.user.screen_name,
+//         tweetText = tweet.text,
+//         regexHello = /hello/i,
+//         regexHi = /hi/i,
+//         regexHey = /hey/i;
 
-    // This only will respond if there is some form of hello that was said, sorry only 3 options for now
-    if (tweetText.match(regexHello) || tweetText.match(regexHey) || tweetText.match(regexHi)) {
-        // This will read the json file and get random greeting phrases
-        fs.readFile(words, 'utf-8', function (err, data) {
-            if (err) throw err;
+//     // This only will respond if there is some form of hello that was said, sorry only 3 options for now
+//     if (tweetText.match(regexHello) || tweetText.match(regexHey) || tweetText.match(regexHi)) {
+//         // This will read the json file and get random greeting phrases
+//         fs.readFile(words, 'utf-8', function (err, data) {
+//             if (err) throw err;
 
-            var greetingsData = JSON.parse(data).greetings,
-                greetingsCount = greetingsData.length,
-                randNum = Math.floor(Math.random() * greetingsCount),
-                randomGreeting = greetingsData[randNum],
-                concactenatedGreeting = randomGreeting + ", " + username;
+//             var greetingsData = JSON.parse(data).greetings,
+//                 greetingsCount = greetingsData.length,
+//                 randNum = Math.floor(Math.random() * greetingsCount),
+//                 randomGreeting = greetingsData[randNum],
+//                 concactenatedGreeting = randomGreeting + ", " + username;
 
-            Tweet.post('statuses/update', { status: concactenatedGreeting}, function(error, tweet, response) {
-                if (error) {
-                    console.log(error);
-                }
-            });
-        }); 
-    }
-});
+//             Tweet.post('statuses/update', { status: concactenatedGreeting}, function(error, tweet, response) {
+//                 if (error) {
+//                     console.log(error);
+//                 }
+//             });
+//         }); 
+//     }
+// });
 
 
-var followHowOften = hoursToMs(20);
+// var followHowOften = hoursToMs(20);
 
-setInterval(function() {
-    Tweet.get('friends/ids', {screen_name: 'ivanaveliskova'}, function(err, data, response) {
-        if(!err) {
-            var ivanaFollows = data.ids;
+// setInterval(function() {
+//     Tweet.get('friends/ids', {screen_name: 'ivanaveliskova'}, function(err, data, response) {
+//         if(!err) {
+//             var ivanaFollows = data.ids;
 
-            Tweet.get('friends/ids', {screen_name: 'IvankaBot'}, function(error, newData, res) {
-                if (!err) {
-                    var botFollows = newData.ids;
-                    Tweet.get('users/show', {screen_name: 'IvankaBot'}, function(errors, botData, responses) {
-                        if (errors) {
-                            console.log(errors);
-                        }
-                        var botID = botData.id;
+//             Tweet.get('friends/ids', {screen_name: 'IvankaBot'}, function(error, newData, res) {
+//                 if (!err) {
+//                     var botFollows = newData.ids;
+//                     Tweet.get('users/show', {screen_name: 'IvankaBot'}, function(errors, botData, responses) {
+//                         if (errors) {
+//                             console.log(errors);
+//                         }
+//                         var botID = botData.id;
 
-                        botFollows.push(botID);
+//                         botFollows.push(botID);
 
-                        var difference = ivanaFollows.diff(botFollows);
+//                         var difference = ivanaFollows.diff(botFollows);
 
-                        var randomNum = Math.floor(Math.random() * difference.length);
+//                         var randomNum = Math.floor(Math.random() * difference.length);
 
-                        var randomUser = difference[randomNum];
-                        Tweet.post('friendships/create', { user_id: randomUser}, function(errorMore, successFollow, respond) {
-                            if (errorMore) {
-                                console.log(errorMore);
-                            }
-                        });
+//                         var randomUser = difference[randomNum];
+//                         Tweet.post('friendships/create', { user_id: randomUser}, function(errorMore, successFollow, respond) {
+//                             if (errorMore) {
+//                                 console.log(errorMore);
+//                             }
+//                         });
                         
-                    });
-                } else {
-                    console.log(error);
-                }
-            });
+//                     });
+//                 } else {
+//                     console.log(error);
+//                 }
+//             });
 
-        } else {
-            console.log(err);
-        }
-    });
-}, followHowOften);
+//         } else {
+//             console.log(err);
+//         }
+//     });
+// }, followHowOften);
 
 
 var retweetJob = hoursToMs(10);
